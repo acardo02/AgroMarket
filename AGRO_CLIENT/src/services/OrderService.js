5b3d0374425a8d4aae3620a5c62ed90255da2540
acardo02 <00037820@uca.edu.sv>
2025-06-26 14:53:42 -0600

    feat: adding button for update order status

--- AGRO_CLIENT/src/services/OrderService.js
+++ AGRO_CLIENT/src/services/OrderService.js
@@ -58,3 +58,24 @@ export const getOrderById = async (orderId) => {
     const data = await response.json()
     return data
 }
+
+export const updateOrderStatus = async (orderId, newStatus) => {
+    const token = localStorage.getItem('token')
+
+    const response = await fetch(`${API_URL}/orders/status/${orderId}`, {
+        method: 'PATCH',
+        headers: {
+            "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+        },
+        body: JSON.stringify({ newStatus })
+    });
+
+    if(!response.ok) {
+        const error = await response.json();
+        throw new Error(error.message || "Error al actulizar el estado de la orden");
+    }
+
+    const data = await response.json();
+    return data;
+}
